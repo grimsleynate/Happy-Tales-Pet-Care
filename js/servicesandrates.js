a6091fe3159458d57e91a4b39d4834ff1faf4c5b
grimsleynate <grimsleynate@gmail.com>
2019-12-04 10:54:20 -0500

    Random Updates

--- js/servicesandrates.js
+++ js/servicesandrates.js
@@ -4,3 +4,13 @@
       document.getElementById("dropdownContentDogWalking").classList.toggle("show");
       document.getElementById("petSittingDropdown").classList.toggle("moveDropdown");
   }
+
+  function dropdownFunctionPetSitting() {
+    document.getElementById("dropdownContentPetSitting").classList.toggle("show");
+    document.getElementById("otherCrittersDropdown").classList.toggle("moveDropdown");
+  }
+
+  function dropdownFunctionOtherCritters() {
+    document.getElementById("dropdownContentOtherCritters").classList.toggle("show");
+    document.getElementById("contactSection").classList.toggle("moveContact");
+  }
