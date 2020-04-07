fee1a067d5e303718928dd84969945e6081c2efb
grimsleynate <grimsleynate@gmail.com>
2020-04-07 18:29:56 -0400

    Finished Static Development of Services Page

--- js/servicesandrates.js
+++ js/servicesandrates.js
@@ -1,16 +1,15 @@
-/*When the user clicks on the button,
-  toggle between hiding and showing the dropdown content */
-  function dropdownFunctionDogWalking() {
-      document.getElementById("dropdownContentDogWalking").classList.toggle("show");
-      document.getElementById("petSittingDropdown").classList.toggle("moveDropdown");
-  }
-
-  function dropdownFunctionPetSitting() {
-    document.getElementById("dropdownContentPetSitting").classList.toggle("show");
-    document.getElementById("otherCrittersDropdown").classList.toggle("moveDropdown");
-  }
-
-  function dropdownFunctionOtherCritters() {
-    document.getElementById("dropdownContentOtherCritters").classList.toggle("show");
-    document.getElementById("contactSection").classList.toggle("moveContact");
-  }
+$(function () {
+  $(document).scroll(function () {
+      //defines $nav as the css class bg-light
+      var $nav = $(".bg-transparent");
+      //defines $navLink as the css class a.nav-link
+      var $navLink = $("a.nav-link");
+      //defines $navBrand as the css class .navbar-brand
+      var $navBrand = $(".navbar-brand");
+      //adds the scrolled css class to the bg-light css class when the user has
+      //scrolled further than the navbar's height.
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $navLink.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $navBrand.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
